94e2682bcb4c001978efda44ff8a36ac5d83edc8
DamirDegenBalaa <116728289+DamirDegenBalaa@users.noreply.github.com>
2024-03-11 06:41:44 +0500

    profile fragment -> edit account info

--- app/src/main/java/com/example/sportifyd/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/sportifyd/ui/profile/ProfileFragment.kt
@@ -1,5 +1,4 @@
 package com.example.sportify.ui.profile
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,16 +6,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.sportifyd.R
 import com.example.sportifyd.databinding.FragmentProfileBinding
+import com.example.sportify.ui.home.HomeFragment
 
 class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
-
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,15 +24,18 @@ class ProfileFragment : Fragment() {
     ): View {
         val profileViewModel =
             ViewModelProvider(this).get(ProfileViewModel::class.java)
-
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textProfile
-        profileViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val navController = findNavController()
+        binding.accountInformation.setOnClickListener {
+            navController.navigate(R.id.action_navigation_profile_to_accountInformationFragment)
+        }
+
     }
 
     override fun onDestroyView() {
