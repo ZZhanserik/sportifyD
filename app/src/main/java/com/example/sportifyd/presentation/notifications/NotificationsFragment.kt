654d20aec9267d714fb857efe138d25d3ab2556a
Zhanserik Zhunissov <200107046@stu.sdu.edu.kz>
2024-04-07 05:26:22 +0500

    notification fix

--- app/src/main/java/com/example/sportifyd/presentation/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/sportifyd/presentation/notifications/NotificationsFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.sportifyd.data.Service
 import com.example.sportifyd.databinding.FragmentNotificationsBinding
+import com.example.sportifyd.entity.SportEvent
+import com.example.sportifyd.presentation.home.adapter.PopularEventAdapter
+import com.example.sportifyd.presentation.home.adapter.PopularEventViewHolder
+import com.firebase.ui.database.FirebaseRecyclerAdapter
+import com.firebase.ui.database.FirebaseRecyclerOptions
 
 class NotificationsFragment : Fragment() {
 
@@ -16,11 +21,30 @@ class NotificationsFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var popularEvents: FirebaseRecyclerOptions<SportEvent>
+    private lateinit var adapterEvents: FirebaseRecyclerAdapter<SportEvent, PopularEventViewHolder>
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-
-
+        loadData()
+        loadDataRV2()
+    }
+    private fun loadData() {
+        popularEvents =
+            FirebaseRecyclerOptions.Builder<SportEvent>()
+                .setQuery(Service.getEventsDataRef(), SportEvent::class.java)
+                .build()
+        adapterEvents = PopularEventAdapter(popularEvents, {})
+        adapterEvents.startListening()
+        binding.popularEventsRv.adapter = adapterEvents
+    }
+    private fun loadDataRV2() {
+        popularEvents =
+            FirebaseRecyclerOptions.Builder<SportEvent>()
+                .setQuery(Service.getEventsDataRef(), SportEvent::class.java)
+                .build()
+        adapterEvents = PopularEventAdapter(popularEvents, {})
+        adapterEvents.startListening()
+        binding.popularOrganizersRv.adapter = adapterEvents
     }
 
     override fun onDestroyView() {
